262cb196dbdadd8cde27b6bc98b41bb5c699b91f
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2020-04-02 11:46:52 +0200

    fixed #2

---
+++ src/main/java/Square.java
@@ -0,0 +1,12 @@
+public class Square {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Square(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/SquareTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareTest {
+
+    @Test
+    void testSquare()
+    {
+        Square s = new Square("prison");
+
+        assertTrue(s.equals("prison"));
+    }
+}
